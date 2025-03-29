fcc75709cc38b9fa1a56796ccb915c6589f6d85e
fati12355 <133292699+fati12355@users.noreply.github.com>
2025-03-29 14:05:19 -0400

    Add files via upload

---
+++ populate_kyoto_singapore_nairobi.sql
@@ -0,0 +1,144 @@
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1021, 1090, '600-8216', 'Shijo-dori', 'Kyoto', 'Kyoto', 'Japan');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1287, 201, 1021, 'Gregory Wheeler', 'NAS01287', 'Engineer, land');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1022, 1091, '600-8216', 'Shijo-dori', 'Kyoto', 'Kyoto', 'Japan');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1288, 201, 1022, 'Charles Lee', 'NAS01288', 'Hotel manager');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1023, 1092, '600-8216', 'Shijo-dori', 'Kyoto', 'Kyoto', 'Japan');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1289, 201, 1023, 'Ronald Watson', 'NAS01289', 'Community pharmacist');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1024, 1093, '600-8216', 'Shijo-dori', 'Kyoto', 'Kyoto', 'Japan');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1290, 201, 1024, 'Wayne Randolph', 'NAS01290', 'General practice doctor');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1025, 1094, '600-8216', 'Shijo-dori', 'Kyoto', 'Kyoto', 'Japan');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1291, 201, 1025, 'Kathryn Turner', 'NAS01291', 'Engineer, technical sales');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1026, 1095, '600-8216', 'Shijo-dori', 'Kyoto', 'Kyoto', 'Japan');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1292, 201, 1026, 'Dawn Bishop', 'NAS01292', 'Intelligence analyst');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1027, 1096, '600-8216', 'Shijo-dori', 'Kyoto', 'Kyoto', 'Japan');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1293, 201, 1027, 'James Parsons', 'NAS01293', 'Waste management officer');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (700, 201, 351, 'Fridge', 3, 'Mountain View', 'Minor scratches', 'Occupied');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (701, 201, 415, 'TV', 3, 'Hot Tub', 'Minor scratches', 'Occupied');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (702, 201, 453, 'Mini Fridge, Heater', 2, 'Hot Tub', 'No damage', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (703, 201, 474, 'TV', 2, 'Mountain View', 'No damage', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (704, 201, 449, 'None', 4, 'Mountain View', 'Minor scratches', 'Available');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (705, 201, 205, 'None', 2, 'Mountain View', 'Minor scratches', 'Occupied');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (706, 201, 201, 'TV, Fridge', 4, 'Ocean View', 'No damage', 'Available');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (707, 201, 322, 'Mini Fridge, Heater', 3, 'Balcony', 'No damage', 'Available');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (708, 201, 178, 'AC', 3, 'Balcony', 'No damage', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (709, 201, 480, 'TV, Fridge', 2, 'Hot Tub', 'Minor scratches', 'Under Maintenance');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1028, 1100, '110001', 'Orchard Road', 'Singapore', 'Central', 'Singapore');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1294, 206, 1028, 'Jason Owen', 'NAS01294', 'Community arts worker');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1029, 1101, '110001', 'Orchard Road', 'Singapore', 'Central', 'Singapore');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1295, 206, 1029, 'Manuel Walters', 'NAS01295', 'Ranger/warden');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1030, 1102, '110001', 'Orchard Road', 'Singapore', 'Central', 'Singapore');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1296, 206, 1030, 'Steven Russell', 'NAS01296', 'Office manager');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1031, 1103, '110001', 'Orchard Road', 'Singapore', 'Central', 'Singapore');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1297, 206, 1031, 'Karen Martinez', 'NAS01297', 'Exhibition designer');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1032, 1104, '110001', 'Orchard Road', 'Singapore', 'Central', 'Singapore');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1298, 206, 1032, 'Megan Fisher', 'NAS01298', 'Sports therapist');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1033, 1105, '110001', 'Orchard Road', 'Singapore', 'Central', 'Singapore');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1299, 206, 1033, 'Fernando Stephens', 'NAS01299', 'Insurance risk surveyor');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1034, 1106, '110001', 'Orchard Road', 'Singapore', 'Central', 'Singapore');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1300, 206, 1034, 'Anthony Fowler', 'NAS01300', 'Chief Technology Officer');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (710, 206, 464, 'TV, Fridge', 1, 'Hot Tub', 'No damage', 'Available');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (711, 206, 168, 'TV', 3, 'Hot Tub', 'No damage', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (712, 206, 138, 'TV, Fridge', 4, 'Balcony', 'No damage', 'Available');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (713, 206, 120, 'Fridge', 2, 'Ocean View', 'Minor scratches', 'Occupied');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (714, 206, 407, 'Fridge', 4, 'Hot Tub', 'Minor scratches', 'Available');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (715, 206, 391, 'Fridge', 2, 'Balcony', 'No damage', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (716, 206, 165, 'Fridge', 2, 'Hot Tub', 'Minor scratches', 'Occupied');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (717, 206, 451, 'Mini Fridge, Heater', 1, 'Balcony', 'No damage', 'Available');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (718, 206, 282, 'Fridge', 1, 'Ocean View', 'Minor scratches', 'Occupied');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (719, 206, 368, 'Mini Fridge, Heater', 1, 'Hot Tub', 'No damage', 'Under Maintenance');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1035, 1110, '00100', 'Kenyatta Avenue', 'Nairobi', 'Nairobi', 'Kenya');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1301, 211, 1035, 'Kathryn Gamble', 'NAS01301', 'Magazine journalist');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1036, 1111, '00100', 'Kenyatta Avenue', 'Nairobi', 'Nairobi', 'Kenya');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1302, 211, 1036, 'Nicholas Wood', 'NAS01302', 'Medical laboratory scientific officer');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1037, 1112, '00100', 'Kenyatta Avenue', 'Nairobi', 'Nairobi', 'Kenya');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1303, 211, 1037, 'Timothy Peck', 'NAS01303', 'Forensic psychologist');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1038, 1113, '00100', 'Kenyatta Avenue', 'Nairobi', 'Nairobi', 'Kenya');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1304, 211, 1038, 'Michael Williams', 'NAS01304', 'Water engineer');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1039, 1114, '00100', 'Kenyatta Avenue', 'Nairobi', 'Nairobi', 'Kenya');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1305, 211, 1039, 'Miguel Wade', 'NAS01305', 'Clothing/textile technologist');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1040, 1115, '00100', 'Kenyatta Avenue', 'Nairobi', 'Nairobi', 'Kenya');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1306, 211, 1040, 'David Foster', 'NAS01306', 'Psychiatric nurse');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1041, 1116, '00100', 'Kenyatta Avenue', 'Nairobi', 'Nairobi', 'Kenya');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1307, 211, 1041, 'Gary Davis', 'NAS01307', 'Chief Operating Officer');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (720, 211, 372, 'TV, Fridge', 4, 'Ocean View', 'No damage', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (721, 211, 219, 'TV', 1, 'Balcony', 'Minor scratches', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (722, 211, 293, 'TV', 2, 'Ocean View', 'No damage', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (723, 211, 437, 'TV, Fridge', 2, 'Balcony', 'Minor scratches', 'Available');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (724, 211, 168, 'AC', 1, 'Ocean View', 'Minor scratches', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (725, 211, 350, 'TV, Fridge', 1, 'Hot Tub', 'Minor scratches', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (726, 211, 158, 'AC', 2, 'Mountain View', 'No damage', 'Available');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (727, 211, 264, 'Fridge', 2, 'Balcony', 'Minor scratches', 'Available');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (728, 211, 284, 'AC', 4, 'Ocean View', 'No damage', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (729, 211, 478, 'AC', 2, 'Balcony', 'No damage', 'Under Maintenance');
---
+++ populate_melbourne_abudhabi_yaounde.sql
@@ -0,0 +1,144 @@
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1100, 1042, '3000', 'Flinders Street', 'Melbourne', 'Victoria', 'Australia');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1350, 216, 1100, 'Mitchell Bradley', 'NAS1350', 'Dispensing optician');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1101, 1043, '3000', 'Flinders Street', 'Melbourne', 'Victoria', 'Australia');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1351, 216, 1101, 'Carolyn Jimenez', 'NAS1351', 'Engineer, electronics');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1102, 1044, '3000', 'Flinders Street', 'Melbourne', 'Victoria', 'Australia');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1352, 216, 1102, 'Julia Miller', 'NAS1352', 'Theatre director');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1103, 1045, '3000', 'Flinders Street', 'Melbourne', 'Victoria', 'Australia');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1353, 216, 1103, 'Elizabeth Moran MD', 'NAS1353', 'Sports therapist');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1104, 1046, '3000', 'Flinders Street', 'Melbourne', 'Victoria', 'Australia');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1354, 216, 1104, 'Anthony Adams', 'NAS1354', 'Loss adjuster, chartered');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1105, 1047, '3000', 'Flinders Street', 'Melbourne', 'Victoria', 'Australia');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1355, 216, 1105, 'Kristin West', 'NAS1355', 'Data scientist');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1106, 1048, '3000', 'Flinders Street', 'Melbourne', 'Victoria', 'Australia');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1356, 216, 1106, 'Emily Brooks', 'NAS1356', 'Toxicologist');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (800, 216, 240, 'TV', 4, 'Ocean View', 'Minor scratches', 'Available');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (801, 216, 137, 'TV', 2, 'Hot Tub', 'Minor scratches', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (802, 216, 103, 'TV, Fridge', 1, 'Balcony', 'No damage', 'Occupied');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (803, 216, 189, 'TV, Fridge', 1, 'Balcony', 'No damage', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (804, 216, 101, 'TV, Fridge', 1, 'Balcony', 'Minor scratches', 'Available');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (805, 216, 225, 'TV, Fridge', 4, 'Ocean View', 'Minor scratches', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (806, 216, 253, 'AC', 3, 'Balcony', 'No damage', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (807, 216, 280, 'TV, Fridge', 2, 'Ocean View', 'No damage', 'Occupied');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (808, 216, 189, 'Fridge', 2, 'Balcony', 'Minor scratches', 'Occupied');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (809, 216, 198, 'Fridge', 1, 'Balcony', 'Minor scratches', 'Occupied');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1107, 1047, '00000', 'Corniche Road', 'Abu Dhabi', 'Abu Dhabi', 'UAE');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1357, 221, 1107, 'Craig Meyer', 'NAS1357', 'Museum education officer');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1108, 1048, '00000', 'Corniche Road', 'Abu Dhabi', 'Abu Dhabi', 'UAE');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1358, 221, 1108, 'Travis Williams', 'NAS1358', 'Product designer');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1109, 1049, '00000', 'Corniche Road', 'Abu Dhabi', 'Abu Dhabi', 'UAE');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1359, 221, 1109, 'David Larsen', 'NAS1359', 'Jewellery designer');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1110, 1050, '00000', 'Corniche Road', 'Abu Dhabi', 'Abu Dhabi', 'UAE');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1360, 221, 1110, 'Janice Cook', 'NAS1360', 'Fish farm manager');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1111, 1051, '00000', 'Corniche Road', 'Abu Dhabi', 'Abu Dhabi', 'UAE');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1361, 221, 1111, 'Hayley Rivera', 'NAS1361', 'Planning and development surveyor');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1112, 1052, '00000', 'Corniche Road', 'Abu Dhabi', 'Abu Dhabi', 'UAE');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1362, 221, 1112, 'Michael Collins', 'NAS1362', 'Engineer, drilling');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1113, 1053, '00000', 'Corniche Road', 'Abu Dhabi', 'Abu Dhabi', 'UAE');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1363, 221, 1113, 'Catherine Petersen', 'NAS1363', 'Designer, jewellery');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (810, 221, 300, 'Fridge', 1, 'Balcony', 'No damage', 'Occupied');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (811, 221, 393, 'Mini Fridge, Heater', 3, 'Balcony', 'Minor scratches', 'Available');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (812, 221, 258, 'TV, Fridge', 4, 'Mountain View', 'No damage', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (813, 221, 118, 'Fridge', 2, 'Hot Tub', 'Minor scratches', 'Occupied');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (814, 221, 394, 'Fridge', 3, 'Ocean View', 'Minor scratches', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (815, 221, 355, 'None', 1, 'Hot Tub', 'Minor scratches', 'Available');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (816, 221, 362, 'None', 1, 'Balcony', 'No damage', 'Available');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (817, 221, 303, 'None', 2, 'Balcony', 'No damage', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (818, 221, 193, 'Mini Fridge, Heater', 3, 'Mountain View', 'Minor scratches', 'Occupied');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (819, 221, 279, 'AC', 2, 'Mountain View', 'Minor scratches', 'Occupied');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1114, 1052, 'CM-4781', 'Avenue Kennedy', 'Yaoundé', 'Centre', 'Cameroon');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1364, 226, 1114, 'Luke Knight', 'NAS1364', 'Planning and development surveyor');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1115, 1053, 'CM-4781', 'Avenue Kennedy', 'Yaoundé', 'Centre', 'Cameroon');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1365, 226, 1115, 'Christopher King Jr.', 'NAS1365', 'Journalist, newspaper');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1116, 1054, 'CM-4781', 'Avenue Kennedy', 'Yaoundé', 'Centre', 'Cameroon');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1366, 226, 1116, 'Catherine Moore', 'NAS1366', 'Bookseller');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1117, 1055, 'CM-4781', 'Avenue Kennedy', 'Yaoundé', 'Centre', 'Cameroon');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1367, 226, 1117, 'Victor Nguyen', 'NAS1367', 'Computer games developer');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1118, 1056, 'CM-4781', 'Avenue Kennedy', 'Yaoundé', 'Centre', 'Cameroon');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1368, 226, 1118, 'Thomas Schroeder', 'NAS1368', 'Hydrogeologist');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1119, 1057, 'CM-4781', 'Avenue Kennedy', 'Yaoundé', 'Centre', 'Cameroon');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1369, 226, 1119, 'Anna Ellison', 'NAS1369', 'Air cabin crew');
+INSERT INTO address (address_id, civic_number, postal_code, street_name, town, province, country)
+VALUES (1120, 1058, 'CM-4781', 'Avenue Kennedy', 'Yaoundé', 'Centre', 'Cameroon');
+INSERT INTO employee (employee_id, hotel_id, address_id, full_name, nas, position)
+VALUES (1370, 226, 1120, 'Katherine Hebert', 'NAS1370', 'Biomedical scientist');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (820, 226, 340, 'None', 2, 'Mountain View', 'No damage', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (821, 226, 215, 'None', 1, 'Mountain View', 'No damage', 'Available');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (822, 226, 375, 'Mini Fridge, Heater', 3, 'Balcony', 'Minor scratches', 'Available');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (823, 226, 157, 'AC', 1, 'Hot Tub', 'Minor scratches', 'Occupied');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (824, 226, 285, 'TV', 1, 'Balcony', 'Minor scratches', 'Available');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (825, 226, 298, 'Fridge', 2, 'Mountain View', 'Minor scratches', 'Occupied');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (826, 226, 108, 'None', 2, 'Ocean View', 'No damage', 'Occupied');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (827, 226, 317, 'TV', 2, 'Ocean View', 'No damage', 'Under Maintenance');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (828, 226, 118, 'TV', 4, 'Hot Tub', 'No damage', 'Occupied');
+INSERT INTO room (room_id, hotel_id, price, appliances, capacity, extras, existing_damage, status)
+VALUES (829, 226, 157, 'TV', 2, 'Balcony', 'No damage', 'Available');
